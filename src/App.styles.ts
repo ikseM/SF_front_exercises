f36a2745e9af303d9eabcb64114f56ae89185cb2
pmezenski <pawel.mezenski@gmail.com>
2020-10-04 22:06:24 +0200

    Initial commit

---
+++ src/App.styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  display: grid;
+  grid-template-rows: 50px 1fr;
+`;
+
+export const Header = styled.div`
+  width: 100%;
+  background-color: #212c40;
+  color: #fff;
+  padding: 5px 3px;
+  font-size: 35px;
+  display: flex;
+  align-items: center;
+`;
+
+export const Main = styled.div``;
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import * as Styled from "./App.styles";
+
+const App = () => {
+  return (
+    <Styled.Container>
+      <Styled.Header>SF Front</Styled.Header>
+      <Styled.Main></Styled.Main>
+    </Styled.Container>
+  );
+};
+
+export default App;
