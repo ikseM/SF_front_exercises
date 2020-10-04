84dbe25a83bb65d84f948063e4775fd6f30c7124
pmezenski <pawel.mezenski@gmail.com>
2020-10-04 22:08:25 +0200

    Initial commit

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,14 +1,11 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  height: 100%;
-  width: 100%;
   display: grid;
-  grid-template-rows: 50px 1fr;
+  grid-template-rows: 50px auto;
 `;
 
 export const Header = styled.div`
-  width: 100%;
   background-color: #212c40;
   color: #fff;
   padding: 5px 3px;
